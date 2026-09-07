5c6f00ecec2269669ee917dba5a37a6152db2039
daniell581 <saidesanchez500@gmail.com>
2026-09-07 11:01:10 -0500

    Refactor game data and modal functionality

--- js/main.js
+++ js/main.js
@@ -99,3 +99,28 @@ $(document).ready(function () {
     const year = new Date().getFullYear();
     $('#year').text(year);
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const videoModal = document.getElementById("videoModal");
+    const videoFrame = document.getElementById("videoFrame");
+    const videoTitle = document.getElementById("videoModalTitle");
+
+    if (videoModal) {
+        videoModal.addEventListener("show.bs.modal", function (event) {
+            const button = event.relatedTarget;
+
+            const videoUrl = button.getAttribute("data-video");
+            const title = button.getAttribute("data-title");
+
+            videoTitle.textContent = title;
+
+            let separator = videoUrl.includes("?") ? "&" : "?";
+            videoFrame.src = videoUrl + separator + "autoplay=1";
+        });
+
+        videoModal.addEventListener("hidden.bs.modal", function () {
+            videoFrame.src = "";
+        });
+    }
+});
